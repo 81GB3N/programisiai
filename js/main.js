cfafa9677f3149caef98aef26a4ff19090577777
81GB3N <jonas.balukonis@gmail.com>
2024-06-02 22:28:37 +0300

    data ir suma

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const daugiau = document.querySelector('.read_more_about');
 const about = document.querySelector('.about');
 
-const body = document.querySelector('body');
-
 daugiau.addEventListener('click', function() {
-    console.log(about.clientHeight);
+    // console.log(about.clientHeight);
     about.scrollIntoView({behavior: "smooth"});
+
+});
+
+const uzsisakyk = document.querySelector('.uzsisakyk');
+const uzsakyti_button = document.querySelector('.uzsakyti-button');
+
+uzsisakyk.addEventListener('click', function() {
+    // console.log(about.clientHeight);
+    uzsakyti_button.scrollIntoView({behavior: "smooth"});
 
 });
