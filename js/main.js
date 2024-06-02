c5139764f0ea28d700278d61a0f3d5479e052a7d
81GB3N <jonas.balukonis@gmail.com>
2024-06-02 18:50:44 +0300

    prettier buttons and design

--- js/main.js
+++ js/main.js
@@ -1,27 +1,10 @@
 const daugiau = document.querySelector('.read_more_about');
 const about = document.querySelector('.about');
 
-// const about_height = about.clientTop;
 const body = document.querySelector('body');
 
 daugiau.addEventListener('click', function() {
     console.log(about.clientHeight);
+    about.scrollIntoView({behavior: "smooth"});
 
-    // about.scrollTop = about.clientHeight;
-    const height = about.clientHeight;
-    
-    // body.scrollBy(0, height);
-    // document.querySelector('body').click();
-    // body.click();
 });
-
-// const next = document.querySelector('.carousel-control-next');
-// const prev = document.querySelector('.carousel-control-prev');
-
-// next.addEventListener('click', function() {
-//     body.click();
-// });
-
-// prev.addEventListener('click', function() {
-//     body.click();
-// });
