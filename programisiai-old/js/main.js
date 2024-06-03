6818841e9c8715442f83dff723fd4659b10e5663
81GB3N <jonas.balukonis@gmail.com>
2024-06-03 19:20:56 +0300

    programisiai-old

---
+++ programisiai-old/js/main.js
@@ -0,0 +1,17 @@
+const daugiau = document.querySelector('.read_more_about');
+const about = document.querySelector('.about');
+
+daugiau.addEventListener('click', function() {
+    // console.log(about.clientHeight);
+    about.scrollIntoView({behavior: "smooth"});
+
+});
+
+const uzsisakyk = document.querySelector('.uzsisakyk');
+const uzsakyti_button = document.querySelector('.uzsakyti-button');
+
+uzsisakyk.addEventListener('click', function() {
+    // console.log(about.clientHeight);
+    uzsakyti_button.scrollIntoView({behavior: "smooth"});
+
+});
